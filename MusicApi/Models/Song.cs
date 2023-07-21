e07ba0182bea1ca61b4a2e5ce202cc5468b740ac
Hasib Ullah <hasibullah04@gmail.com>
2023-07-21 15:10:47 +0600

    Added song model

---
+++ MusicApi/Models/Song.cs
@@ -0,0 +1,9 @@
+﻿namespace MusicApi.Models
+{
+    public class Song
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Language { get; set; }
+    }
+}
